d6178400b1d0c9cdf79cb71078ec1da09e7dea9d
JaeDong_95 <badzoo@naver.com>
2021-10-31 03:09:32 +0900

    Update tmp.py

--- tmp.py
+++ tmp.py
@@ -1,9 +1,6 @@
-from datetime import date 
-
-a_year = int(input('년도를 입력하시오: '))
-a_month = int(input('월을 입력하시오: '))
-a_day = int(input('일을 입력하시오: '))
-
-day_of_week = ['월요일', '화요일', '수요일', '목요일', '금요일', '토요일', '일요일']
-print(day_of_week[date(a_year, a_month, a_day).weekday()])
-# print(f'{a_year}년 {a_month}월 {a_day}일은 {day_of_week[date(a_year, a_month, a_day).weekday]}')
+def factorial(n: int):
+    if n == 0 or n == 1:
+        return 1 
+    return n * factorial(n-1)
+    
+print(factorial(3))
