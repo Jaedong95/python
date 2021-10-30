9f48be0c06a7db12b4b660b35ffd6f7e833c46ea
JaeDong_95 <badzoo@naver.com>
2021-10-31 03:04:02 +0900

    Update tmp.py

--- tmp.py
+++ tmp.py
@@ -1,13 +1,9 @@
-score = int(input('점수를 입력하세요: '))
-if score >= 81 and score <= 100:
-    print('A')
-elif score >= 61 and score <= 80:
-    print('B')
-elif score >= 41 and score <= 60: 
-    print('C')
-elif score >= 21 and score <= 40:
-    print('D')
-elif score >= 0 and score <= 20:
-    print('F')
-else:
-    print('제대로 된 점수를 입력하세요.')
+from datetime import date 
+
+a_year = int(input('년도를 입력하시오: '))
+a_month = int(input('월을 입력하시오: '))
+a_day = int(input('일을 입력하시오: '))
+
+day_of_week = ['월요일', '화요일', '수요일', '목요일', '금요일', '토요일', '일요일']
+print(day_of_week[date(a_year, a_month, a_day).weekday()])
+# print(f'{a_year}년 {a_month}월 {a_day}일은 {day_of_week[date(a_year, a_month, a_day).weekday]}')
