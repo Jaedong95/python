4db0a58db61de0f89df829fd59182dad67ebc646
JaeDong_95 <badzoo@naver.com>
2021-10-31 02:27:27 +0900

    Update tmp.py

--- tmp.py
+++ tmp.py
@@ -1,4 +1,3 @@
-namelist = ['Mary', 'Sams', 'Aimy', 'Tom', 'Michale', 'Bob', 'Kelly'] 
-
-namelist.sort()
-print(namelist)
+personal_num = input("주민등록번호를 입력하세요: ")
+number = personal_num.split('-')
+print("남자" if number[1][0] in ['1', '3'] else "여자")
