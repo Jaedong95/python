e260133faf3c045e1b91dbbda3199329206160ab
JaeDong_95 <badzoo@naver.com>
2021-10-31 02:36:28 +0900

    Update tmp.py

--- tmp.py
+++ tmp.py
@@ -1,3 +1,3 @@
-personal_num = input("주민등록번호를 입력하세요: ")
-number = personal_num.split('-')
-print("남자" if number[1][0] in ['1', '3'] else "여자")
+a = ['alpha', 'bravo', 'charlie', 'delta', 'echo', 'foxtrot', 'golf', 'hotel', 'india']
+
+print([data for data in a if len(data) == 7])
