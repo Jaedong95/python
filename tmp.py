e9394b4e3b05ced70f800e59a39d67483e4cd8a9
JaeDong_95 <badzoo@naver.com>
2021-10-31 02:45:12 +0900

    Update tmp.py

--- tmp.py
+++ tmp.py
@@ -1,3 +1,13 @@
-a = ['alpha', 'bravo', 'charlie', 'delta', 'echo', 'foxtrot', 'golf', 'hotel', 'india']
-
-print([data for data in a if len(data) == 7])
+score = int(input('점수를 입력하세요: '))
+if score >= 81 and score <= 100:
+    print('A')
+elif score >= 61 and score <= 80:
+    print('B')
+elif score >= 41 and score <= 60: 
+    print('C')
+elif score >= 21 and score <= 40:
+    print('D')
+elif score >= 0 and score <= 20:
+    print('F')
+else:
+    print('제대로 된 점수를 입력하세요.')
