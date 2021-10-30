fa6536930eb58934aaca1032f6b42d5e7363ee20
JaeDong_95 <badzoo@naver.com>
2021-10-31 01:55:51 +0900

    Update tmp.py

--- tmp.py
+++ tmp.py
@@ -1,5 +1,5 @@
 width = int(input("가로의 숫자를 입력하시오: "))
 height = int(input("세로의 숫자를 입력하시오: "))
 
-for i in range(width):
-    print("*" * height)
+value = width * height * 1/2 
+print(f'가로 {width}, 세로 {height}인 삼각형의 넓이: {value}')
