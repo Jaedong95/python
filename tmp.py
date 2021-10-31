843e9a1d0fd7e7114546e041797c901c754258e7
JaeDong_95 <badzoo@naver.com>
2021-10-31 19:44:32 +0900

    Update tmp.py

--- tmp.py
+++ tmp.py
@@ -1,6 +1,21 @@
-def factorial(n: int):
-    if n == 0 or n == 1:
-        return 1 
-    return n * factorial(n-1)
+class Card:
+    price = 0
+    def charge(self, p:int):
+        self.price += p 
     
-print(factorial(3))
+    def consume(self, p:int):
+        self.price -= p 
+    
+    def print(self):
+        print(f"잔액이 {self.price}원 입니다.")
+
+card = Card()
+user_answer = input('충전 / 소비 ? ')
+if user_answer == '충전':
+    price = int(input('충전할 금액을 입력하세요: '))
+    card.charge(price)
+elif user_answer == '소비':
+    price = int(input('소비할 금액을 입력하세요: '))
+    card.consume(price)
+
+card.print()
