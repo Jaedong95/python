07fc862ac9eef0263ea0d268a933d76c940419cc
JaeDong_95 <badzoo@naver.com>
2021-11-06 11:57:01 +0900

    Update print_diamond.py

--- print_diamond.py
+++ print_diamond.py
@@ -12,7 +12,7 @@ def pattern(n):
     for i in range(n , -1, -1):
         for j in range(k , 0 , -1): 
             print(end=" ")
-        assadas
+        assada
         k = k + 1 
         for j in range(0 , i + 1):
             print("* ", end="")
