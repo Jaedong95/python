9196c7fec26adacd0487c8e47baf3b6cf4ebbbb6
SleepyJD <badzoo@naver.com>
2022-03-05 18:31:33 +0900

    update

---
+++ Datascience/dynamic_programming.py
@@ -0,0 +1,26 @@
+''' 피보나치 수열 '''
+# 반복문 
+def fibo(n):
+    i = 1 
+    bunny, rabby = 1, 0 
+    while i < n: 
+        i += 1
+        bunny, rabby = rabby, bunny + rabby
+    return bunny + rabby 
+
+
+def fibo(n):
+    bunny, rabby = 1, 0
+    for _ in range(2, n + 1):
+        bunny, rabby = rabby, bunny + rabby 
+    return bunny + rabby
+
+# 재귀 
+def fibseq(n):
+    fibs = [0, 1]
+    for k in range(2, n+1):
+        fibs.append(fibs[k-1] + fibs[k-2])
+    return fibs 
+
+def fib(n):
+    return fibseq(n)[-1]
