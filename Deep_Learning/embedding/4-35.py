8440e7f7260f0253ba10091035c3287a9cbf7c7e
JaeDong_95 <badzoo@naver.com>
2022-03-27 18:04:49 +0900

    .

--- Deep_Learning/embedding/4-35.py
+++ Deep_Learning/embedding/4-35.py
@@ -1,4 +1,9 @@
 '''
 단어 유사도 평가 수행 코드 
 '''
+from models.word_eval import WordEmbeddingEvaluator 
+
+model = WordEmbeddingEvaluator('/notebooks/embedding/data/word-embeddings/\
+    word2vec/word2vec', method='word2vec', dim=100, tokenizer_name='mecab')
+
 model.most_similar('희망', topn=5)
--- Deep_Learning/embedding/4-39.py
+++ Deep_Learning/embedding/4-39.py
@@ -1,6 +1,5 @@
 '''
-평가 클래스 로드 + 단어 임베딩의 t-SNE 2차원 시각화 
-+ 단어 벡터 간 유사도 시각화 
+평가 클래스 로드 
 '''
 from models.word_eval import WordEmbeddingEvaluator 
 
