427db7fbc82df24c4580c86988506bbe164ea37e
JaeDong_95 <badzoo@naver.com>
2022-03-28 03:07:45 +0900

    .

---
+++ Deep_Learning/embedding/chapter_05/5-22.py
@@ -0,0 +1,6 @@
+'''
+LDA 평가 모듈 선언 
+'''
+from models.sent_eval import LDAEvaluator 
+
+model = LDAEvaluator('/notebooks/embedding/data/sentence-embeddings/lda/lda')
---
+++ Deep_Learning/embedding/chapter_05/5-24.py
@@ -0,0 +1,5 @@
+'''
+토픽별 단어 확인 
+'''
+# 토픽 ID를 인자로 주어 해당 토픽 확률 값이 가장 높은 단어 출력 
+model.show_topic_words(topic_id=0)
