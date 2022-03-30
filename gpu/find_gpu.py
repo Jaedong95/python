7794dacb5a382da4edc3e5098f283333682d4fa0
Jaedong <badzoo@naver.com>
2022-03-30 16:20:13 +0900

    Update find_gpu.py

--- gpu/find_gpu.py
+++ gpu/find_gpu.py
@@ -17,3 +17,22 @@ print('Number of devices: {}'.format(strategy.num_replicas_in_sync))
 from tensorflow.python.client import device_lib
 
 print(device_lib.list_local_devices())
+
+
+''' 4. CPU 전체 정보 확인 '''
+$ cat /proc/cpuinfo 
+
+''' 5. CPU 코어 수 확인 ''' 
+$ cat /proc/cpuinfo | grep processor | wc -l 
+
+''' 6. 논리 코어 수 확인 ''' 
+$ grep -c processeor /proc/cpuinfo 
+
+''' 7. 물리 CPU 개수 ''' 
+$ grep "physical id" /proc/cpuinfo | sort -u | wc -l
+
+''' 8. CPU당 물리 코어 수 확인 ''' 
+$ grep "cpu cores" /proc/cpuinfo | tail -1
+
+''' 9. 우분투 그래픽 카드 GPU 확인 방법 '''
+$ lspci | grep -i VGA
