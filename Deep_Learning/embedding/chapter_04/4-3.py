a8604067a4c8354be6cd2cfc018f6092224b1a3e
JaeDong_95 <badzoo@naver.com>
2022-04-02 01:14:51 +0900

    Update 4-3.py

--- Deep_Learning/embedding/chapter_04/4-3.py
+++ Deep_Learning/embedding/chapter_04/4-3.py
@@ -9,4 +9,4 @@ from gensim.models import Word2Vec
 corpus = [sent.strip().split(' ') for sent in open(corpus_fname, 'r').readlines()] 
  # size: Word2Vec 임베딩의 차원 수,  sg=1  -> Skip-gram, 0  -> CBOW 모델로 학습 
 model = Word2Vec(corpus, size=100, workers=4, sg=1) 
-model.save(model_fname)   # 모델 저장 asd
+model.save(model_fname)   # 모델 저장 as
